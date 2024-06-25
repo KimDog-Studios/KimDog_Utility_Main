cc769c44707c8133c7a99f8bd78b6e1d244e6df5
KimDog <kimialbans54@gmail.com>
2024-06-26 00:57:15 +0100

    Made it clear console

--- lib/compiler.ps1
+++ lib/compiler.ps1
@@ -104,6 +104,7 @@ else {
 Write-Output "Installation completed. Please Wait..." 
 Start-Sleep -Seconds 5
 
+Clear-Host
 # GitHub URL
 $gistUrl = "https://raw.githubusercontent.com/KimDog-Studios/KimDog_Utility_Main/main/lib/main.ps1"
 Invoke-WebRequest -URI $gistUrl | Invoke-Expression
