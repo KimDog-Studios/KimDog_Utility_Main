586eb910d0b53d02ba818b5b84b65bfe5f1e8ff8
KimDog <kimialbans54@gmail.com>
2024-06-25 19:00:20 +0100

    update

---
+++ lib/WindowsOptions.ps1
@@ -0,0 +1,41 @@
+# Define the base directory
+$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+$cabFiles = "$scriptDir\lib"
+
+function Documents {
+    $cabFile = "$cabFiles\Documents_001.cab"
+    $extractPath = "$env:USERPROFILE\Documents\"
+
+    # Check if the CAB file exists and extract it
+    if (Test-Path $cabFile) {
+        Expand -F:* $cabFile $extractPath
+        Write-Host "Documents extracted successfully."
+    } else {
+        Write-Host "Documents CAB file does not exist."
+        return
+    }
+
+    # Proceed with copying items from the extracted Documents folder, excluding the extraction folder
+    Write-Host "Copying Items from Extracted Document Folder to the Users Documents! Please wait..."
+    $source = Get-ChildItem -Path $extractPath -Exclude "Documents"
+    $destination = "$env:USERPROFILE\Documents"
+    foreach ($item in $source) {
+        Copy-Item -Path $item.FullName -Destination $destination -Recurse -Force
+    }
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Failed to copy files!"
+    } else {
+        Write-Host "Finished copying all the Files from Extracted Documents folder..."
+    }
+
+    # Optional: Clean up extracted files after copying
+    # Remove-Item -Path $extractPath -Recurse -Force
+    # Write-Host "Cleanup of extracted files complete."
+
+    Start-Sleep -Seconds 5
+    Clear-Host
+}
+
+function Software {
+    
+}
