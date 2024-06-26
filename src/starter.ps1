8ac4c67d493dd1d3b9067952c159853e574d6534
KimDog <kimialbans54@gmail.com>
2024-06-26 13:52:50 +0100

    Update

---
+++ src/starter.ps1
@@ -0,0 +1,11 @@
+# URL of the remote script on GitHub
+$remoteScriptUrl = "https://raw.githubusercontent.com/KimDog-Studios/KimDog_Utility_Main/main/src/compiler.ps1"
+
+# Download the remote script content
+$scriptContent = Invoke-WebRequest -Uri $remoteScriptUrl -UseBasicParsing | Select-Object -ExpandProperty Content
+
+# Encode the script content to Base64
+$encodedScript = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($scriptContent))
+
+# Start the script with elevated privileges
+Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -EncodedCommand $encodedScript" -Verb runAs
