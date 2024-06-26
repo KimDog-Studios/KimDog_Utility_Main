2eed29ec1fcd16131f00e2343b845dc6d54903db
KimDog <kimialbans54@gmail.com>
2024-06-26 13:18:08 +0100

    Admin Test

--- src/compiler.ps1
+++ src/compiler.ps1
@@ -8,8 +8,12 @@ function Test-Administrator {
 if (-not (Test-Administrator)) {
     Write-Output "Script is not running as administrator. Restarting with elevated privileges..."
     Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    Start-Sleep -Seconds 5  # Give some time for the elevated process to start
     exit
 }
+
+# Confirming script is running with elevated privileges
+Write-Output "Script is running as administrator."
 
 # Check if Winget is installed
 $wingetInstalled = $false
